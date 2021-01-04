112c5b910daa32eeb28212a8593e5cd96027f408
KuzMich1994 <drumer19940310@mail.ru>
2021-01-04 17:21:59 +0300

    Выполнил задание 14 урока

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize, position, top, left, right, bottom) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.position = position;
+  this.top = top;
+  this.left = left;
+  this.right = right;
+  this.bottom = bottom;
+
+
 }
 
 let newDomElement1 = new DomElement('.block', '200px', '300px', 'red', '26px');
 let newDomElement2 = new DomElement('#paragraph', '50px', '1000px', 'blue', '30px');
+let newDomElement3 = new DomElement('.second-block', '100px', '100px', 'green', '10px', 'absolute', 0, 0, 0, 0);
 
 DomElement.prototype.createElement = function() {
   if (newDomElement1.selector.startsWith('.')) {
@@ -29,7 +37,35 @@ DomElement.prototype.createElement = function() {
     document.body.prepend(paragraph);
   }
 };
+DomElement.prototype.eventListener = function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    let div = document.createElement('div');
+    div.classList.add(newDomElement3.selector.substring(1));
+    div.style.cssText = `width:${newDomElement3.width}; height:${newDomElement3.height};
+    background-color:${newDomElement3.bg}; font-size:${newDomElement3.fontSize}; position:${newDomElement3.position};`;
+    document.querySelector('.block').after(div);
+    document.addEventListener('keydown', function(event) {
+      if (event.code === 'ArrowUp') {
+        div.style.bottom = newDomElement3.top * 10 + 'px';
+        return newDomElement3.top++;
+      }
+      if (event.code === 'ArrowRight') {
+        div.style.left = newDomElement3.right * 10 + 'px';
+        return newDomElement3.right++;
+      }
+      if (event.code === 'ArrowDown') {
+        div.style.bottom = newDomElement3.top * 10 + 'px';
+        return newDomElement3.top--;
+      }
+      if (event.code === 'ArrowLeft') {
+        div.style.left = newDomElement3.right * 10 + 'px';
+        return newDomElement3.right--;
+      }
+    });
+  });
+};
 
 
 DomElement.prototype.createElement();
+DomElement.prototype.eventListener();
 
